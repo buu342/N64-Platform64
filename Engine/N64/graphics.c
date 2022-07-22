907b04b91036e8d2af8f5cbcf76cdd4601f68cb6
Buu342 <buu342@hotmail.com>
2022-07-22 23:41:03 +0100

    And now properly multi-threaded

--- Engine/N64/graphics.c
+++ Engine/N64/graphics.c
@@ -9,6 +9,22 @@ Handles graphics rendering
 #include "osconfig.h"
 #include "graphics.h"
 
+/*********************************
+           Definitions
+*********************************/
+
+#define SIZE_MSGQUEUE_GRAPHICS  8
+    
+    
+/*********************************
+             Structs
+*********************************/
+
+typedef struct 
+{
+    u8 color;
+} t_gfxtask;
+
 
 /*********************************
         Function Prototypes
@@ -22,6 +38,9 @@ static void threadfunc_graphics(void *arg);
 *********************************/
 
 static OSThread	s_threadstruct_graphics;
+
+static OSMesgQueue s_msgqueue_graphics;
+static OSMesg      s_msgbuffer_graphics[SIZE_MSGQUEUE_GRAPHICS];
 
 
 /*==============================
@@ -45,32 +64,43 @@ void graphics_initialize()
 
 static void threadfunc_graphics(void *arg)
 {
+    t_gfxtask* task;
+    
+    // Initialize the thread
     debug_printf("Created graphics thread\n");
+    osCreateMesgQueue(&s_msgqueue_graphics, s_msgbuffer_graphics, SIZE_MSGQUEUE_GRAPHICS);
     
     // Spin this thread forever
     while (1)
     {
+        // Wait for a graphics message to arrive
+        osRecvMesg(&s_msgqueue_graphics, (OSMesg *)&task, OS_MESG_BLOCK);
         debug_printf("Graphics thread loop start\n");
         
-        debug_printf("Stopping graphics thread\n");
-        osStopThread(&s_threadstruct_graphics);
+        // Paint our framebuffer green
+        memset(FRAMEBUFF_ADDR1_SD, task->color, FRAMEBUFF_SIZE_SD);
+        
+        // Display the framebuffer
+        debug_printf("Framebuffer colored with %2x\n", task->color);
+        osViSwapBuffer(FRAMEBUFF_ADDR1_SD);
     }
 }
 
 
 /*==============================
     graphics_renderscene
-    Renders the scene
+    Queues a scene render
     @param The color to set the framebuffer
 ==============================*/
 
 void graphics_renderscene(const u8 a_color)
 {
-    debug_printf("Rendering scene\n");
-
-    // Paint our framebuffer green
-    memset(FRAMEBUFF_ADDR1_SD, a_color, FRAMEBUFF_SIZE_SD);
+    t_gfxtask task;
+    debug_printf("Requesting scene render with color %2x\n", a_color);
     
-    // Display the framebuffer
-    osViSwapBuffer(FRAMEBUFF_ADDR1_SD);
+    // Specify the task for the graphics thread to execute
+    task.color = a_color;
+    
+    // Send the task to the graphics thread
+    osSendMesg(&s_msgqueue_graphics, (OSMesg *)&task, OS_MESG_BLOCK);
 }
