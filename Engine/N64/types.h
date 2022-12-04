c324cfd5b0ca39fc425ce5eb14f12957d8bc00f9
Buu342 <buu342@hotmail.com>
2022-12-04 00:57:09 +0000

    Added time conversion macros

--- Engine/N64/types.h
+++ Engine/N64/types.h
@@ -1,8 +1,11 @@
 #ifndef PLATFORM64_TYPES_H
 #define PLATFORM64_TYPES_H
+    
+    #define SEC_TO_USEC(a)  (a*1000000)
+    #define USEC_TO_SEC(a)  (((f32)a)*0.000001)
 
-    #define FALSE 0
-    #define TRUE 1
+    #define FALSE  0
+    #define TRUE   1
 
     typedef u8 bool;
 
