af1553f756987d2b145d8df6d553cc1d611b2261
Buu342 <buu342@hotmail.com>
2025-06-24 12:51:55 +0100

    Added deserialization code

--- Tools/SDK/serializer.cpp
+++ Tools/SDK/serializer.cpp
@@ -67,33 +67,63 @@ void serialize_f32(std::vector<uint8_t>* buff, float val)
     #endif
 }
 
-void serialize_string(std::vector<uint8_t>* buff, const char* str)
-{
-    int strb_len = strlen(str)+1;
-
-    // Write the string length and then the data itself
-    serialize_u32(buff, strb_len);
-    for (int i=0; i<strb_len; i++)
-        buff->push_back(str[i]);
-
-    // Zero pad to the nearest 4 bytes
-    if ((strb_len%4) != 0)
-        for (int i=0; i<(4-(strb_len%4)); i++)
-            buff->push_back('\0');
-}
-
 void serialize_wxstring(std::vector<uint8_t>* buff, wxString str)
 {
     const char* strb = str.mb_str(wxConvUTF8);
-    int strb_len = strlen(strb)+1;
+    int len = strlen(strb)+1; // TODO: NULL check might not work for UTF-8
 
     // Write the string length and then the data itself
-    serialize_u32(buff, strb_len);
-    for (int i=0; i<strb_len; i++)
+    serialize_u32(buff, len);
+    for (int i=0; i<len; i++)
         buff->push_back(strb[i]);
+}
 
-    // Zero pad to the nearest 4 bytes
-    if ((strb_len%4) != 0)
-        for (int i=0; i<(4-(strb_len%4)); i++)
-            buff->push_back('\0');
+uint32_t deserialize_header(uint8_t* buff, uint32_t pos, char* header, uint32_t* version)
+{
+    int len = strlen((char*)buff)+1; // TODO: NULL check might not work for UTF-8
+    memcpy(header, buff+pos, len);
+    pos += len;
+    pos = deserialize_u32(buff, pos, version);
+    return pos;
+}
+
+uint32_t deserialize_u8(uint8_t* buff, uint32_t pos, uint8_t* val)
+{
+    memcpy(val, buff+pos, sizeof(uint8_t));
+    pos += sizeof(uint8_t);
+    return pos;
+}
+
+uint32_t deserialize_u32(uint8_t* buff, uint32_t pos, uint32_t* val)
+{
+    if (pos%4 != 0)
+        pos += 4 - (pos%4);
+    memcpy(val, buff+pos, sizeof(uint32_t));
+    #if (IS_BIG_ENDIAN)
+        *val = ((*val>>24)&0xFF) | ((*val<<8)&0xFF0000) | ((*val>>8)&0xFF00) | ((*val<<24)&0xFF000000);
+    #endif
+    pos += sizeof(uint32_t);
+    return pos;
+}
+
+uint32_t deserialize_f32(uint8_t* buff, uint32_t pos, float* val)
+{
+    if (pos%4 != 0)
+        pos += 4 - (pos%4);
+    memcpy(val, buff+pos, sizeof(float));
+    #if (IS_BIG_ENDIAN)
+        *val = ((*val>>24)&0xFF) | ((*val<<8)&0xFF0000) | ((*val>>8)&0xFF00) | ((*val<<24)&0xFF000000);
+    #endif
+    pos += sizeof(float);
+    return pos;
+}
+
+uint32_t deserialize_wxstring(uint8_t* buff, uint32_t pos, wxString* str)
+{
+    wxString temp;
+    uint32_t len;
+    pos = deserialize_u32(buff, pos, &len);
+    *str = wxString::FromUTF8((char*)buff+pos);
+    pos += len;
+    return pos; 
 }
--- Tools/SDK/serializer.h
+++ Tools/SDK/serializer.h
@@ -6,5 +6,10 @@ void serialize_header(std::vector<uint8_t>* buff, const char* header, uint32_t v
 void serialize_u8(std::vector<uint8_t>* buff, uint8_t val);
 void serialize_u32(std::vector<uint8_t>* buff, uint32_t val);
 void serialize_f32(std::vector<uint8_t>* buff, float val);
-void serialize_string(std::vector<uint8_t>* buff, const char* str);
 void serialize_wxstring(std::vector<uint8_t>* buff, wxString str);
+
+uint32_t deserialize_header(uint8_t* buff, uint32_t pos, char* header, uint32_t* version);
+uint32_t deserialize_u8(uint8_t* buff, uint32_t pos, uint8_t* val);
+uint32_t deserialize_u32(uint8_t* buff, uint32_t pos, uint32_t* val);
+uint32_t deserialize_f32(uint8_t* buff, uint32_t pos, float* val);
+uint32_t deserialize_wxstring(uint8_t* buff, uint32_t pos, wxString* str);
