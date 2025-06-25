f8c7c7eac5144a14ef980ff75075e5055bb8272c
Buu342 <buu342@hotmail.com>
2025-06-25 14:28:46 +0100

    Image asset

--- Tools/SDK/Tools/Image/asset_image.cpp
+++ Tools/SDK/Tools/Image/asset_image.cpp
@@ -1,11 +1,116 @@
 #include "asset_image.h"
+#include "../../serializer.h"
+#include <wx/msgdlg.h>
+
+#define P64ASSET_HEADER "P64RAWIMG"
+#define P64ASSET_VERSION 1
+
 
 P64Asset_Image::P64Asset_Image()
 {
-    
+    this->m_SourcePath = "";
+    this->m_ResizeMode = RESIZETYPE_NONE;
+    this->m_CustomSize = wxPoint(0, 0);
+    this->m_Alignment = RESIZEALIGN_CENTER;
+    this->m_ResizeFill = RESIZEFILL_INVISIBLE;
+    this->m_ImageFormat = FMT_RGBA16;
+    this->m_TilingX = TILING_MIRROR;
+    this->m_TilingY = TILING_MIRROR;
+    this->m_MaskStart = wxPoint(0, 0);
+    this->m_UseMipmaps = false;
+    this->m_Quantization = QUANTIZATION_MEDIANCUT;
+    this->m_AlphaMode = ALPHA_NONE;
+    this->m_AlphaColor = wxColor(0, 0, 0);
+    this->m_AlphaPath = "";
+
+    this->m_Bitmap = wxBitmap();
+    this->m_BitmapAlpha = wxBitmap();
+    this->m_FinalTexels = NULL;
 }
 
 P64Asset_Image::~P64Asset_Image()
 {
 
+}
+
+std::vector<uint8_t> P64Asset_Image::Serialize()
+{
+    std::vector<uint8_t> data;
+
+    serialize_header(&data, P64ASSET_HEADER, P64ASSET_VERSION);
+    serialize_wxstring(&data, this->m_SourcePath.GetFullPath());
+    serialize_u8(&data, this->m_ResizeMode);
+    serialize_u32(&data, this->m_CustomSize.x);
+    serialize_u32(&data, this->m_CustomSize.y);
+    serialize_u8(&data, this->m_Alignment);
+    serialize_u8(&data, this->m_ResizeFill);
+    serialize_u8(&data, this->m_ImageFormat);
+    serialize_u8(&data, this->m_TilingX);
+    serialize_u8(&data, this->m_TilingY);
+    serialize_u32(&data, this->m_MaskStart.x);
+    serialize_u32(&data, this->m_MaskStart.y);
+    serialize_u8(&data, this->m_UseMipmaps);
+    serialize_u8(&data, this->m_Quantization);
+    serialize_u8(&data, this->m_AlphaMode);
+    serialize_u8(&data, this->m_AlphaColor.Red());
+    serialize_u8(&data, this->m_AlphaColor.Green());
+    serialize_u8(&data, this->m_AlphaColor.Blue());
+    serialize_wxstring(&data, this->m_AlphaPath.GetFullPath());
+
+    return data;
+}
+
+static P64Asset_Image* Deserialize(std::vector<uint8_t> bytes)
+{
+    P64Asset_Image* asset;
+    wxString temp_str;
+    uint32_t temp_32;
+    uint8_t temp_8[3];
+    char header[16];
+    uint8_t* bytesptr = &bytes[0];
+    uint32_t pos = 0;
+
+    pos = deserialize_header(bytesptr, pos, header, &temp_32);
+    if (!strcmp(header, P64ASSET_HEADER))
+    {
+        wxMessageDialog dialog(NULL, "File is not an image type", "Error deserializing", wxCENTER | wxNO_DEFAULT | wxOK | wxICON_ERROR);
+        dialog.ShowModal();
+        return NULL;
+    }
+    if (temp_32 > P64ASSET_VERSION)
+    {
+        wxMessageDialog dialog(NULL, "File is a more recent, unsupported version", "Error deserializing", wxCENTER | wxNO_DEFAULT | wxOK | wxICON_ERROR);
+        dialog.ShowModal();
+        return NULL;
+    }
+
+    asset = new P64Asset_Image();
+    pos = deserialize_wxstring(bytesptr, pos, &temp_str);
+    asset->m_SourcePath = wxFileName(temp_str);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_ResizeMode);
+    pos = deserialize_u32(bytesptr, pos, (uint32_t*)&asset->m_CustomSize.x);
+    pos = deserialize_u32(bytesptr, pos, (uint32_t*)&asset->m_CustomSize.y);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_Alignment);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_ResizeFill);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_ImageFormat);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_TilingX);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_TilingY);
+    pos = deserialize_u32(bytesptr, pos, (uint32_t*)&asset->m_MaskStart.x);
+    pos = deserialize_u32(bytesptr, pos, (uint32_t*)&asset->m_MaskStart.y);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_UseMipmaps);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_Quantization);
+    pos = deserialize_u8(bytesptr, pos, (uint8_t*)&asset->m_AlphaMode);
+    pos = deserialize_u8(bytesptr, pos, &temp_8[0]);
+    pos = deserialize_u8(bytesptr, pos, &temp_8[1]);
+    pos = deserialize_u8(bytesptr, pos, &temp_8[2]);
+    asset->m_AlphaColor.Set(temp_8[0], temp_8[1], temp_8[2], 255);
+    pos = deserialize_wxstring(bytesptr, pos, &temp_str);
+    asset->m_AlphaPath = wxFileName(temp_str);
+
+    return asset;
+}
+
+wxColor* P64Asset_Image::RegenerateFinal()
+{
+    return NULL;
 }
--- Tools/SDK/Tools/Image/asset_image.h
+++ Tools/SDK/Tools/Image/asset_image.h
@@ -3,27 +3,92 @@
 typedef struct IUnknown IUnknown;
 
 #include <wx/filename.h>
+#include <wx/bitmap.h>
+#include <vector>
+#include <stdint.h>
 
 typedef enum {
-    G_IM_FMT_RGBA32,
-    G_IM_FMT_RGBA16,
-    G_IM_FMT_YUV16,
-    G_IM_FMT_CI8,
-    G_IM_FMT_CI4,
-    G_IM_FMT_IA16,
-    G_IM_FMT_IA8,
-    G_IM_FMT_IA4,
-    G_IM_FMT_I8,
-    G_IM_FMT_I4,
-} N64ImageType;
+    RESIZETYPE_NONE = 0,
+    RESIZETYPE_POWER2,
+    RESIZETYPE_CUSTOM,
+} P64Img_Resize;
+
+typedef enum {
+    RESIZEALIGN_TOPLEFT = 0,
+    RESIZEALIGN_TOPMIDDLE,
+    RESIZEALIGN_TOPRIGHT,
+    RESIZEALIGN_MIDDLELEFT,
+    RESIZEALIGN_CENTER,
+    RESIZEALIGN_MIDDLERIGHT,
+    RESIZEALIGN_BOTTOMLEFT,
+    RESIZEALIGN_BOTTOMMIDDLE,
+    RESIZEALIGN_BOTTOMRIGHT,
+} P64Img_Alignment;
+
+typedef enum {
+    RESIZEFILL_INVISIBLE = 0,
+    RESIZEFILL_EDGE,
+    RESIZEFILL_REPEAT,
+} P64Img_Fill;
+
+typedef enum {
+    FMT_RGBA32 = 0,
+    FMT_RGBA16,
+    //FMT_YUV16,
+    FMT_IA16,
+    FMT_IA8,
+    FMT_IA4,
+    FMT_I8,
+    FMT_I4,
+    FMT_CI8,
+    FMT_CI4,
+} P64Img_Format;
+
+typedef enum {
+    TILING_MIRROR = 0,
+    TILING_WRAP,
+    TILING_CLAMP,
+} P64Img_Tiling;
+
+typedef enum {
+    QUANTIZATION_NONE = 0,
+    QUANTIZATION_MEDIANCUT,
+    QUANTIZATION_MEDIANCUT_FLOYDSTEINBERG,
+} P64Img_QuantizationMode;
+
+typedef enum {
+    ALPHA_NONE = 0,
+    ALPHA_MASK,
+    ALPHA_CUSTOM,
+    ALPHA_EXTERNALMASK,
+} P64Img_AlphaMode;
 
 class P64Asset_Image
 {
     private:
-        wxFileName m_SourcePath;
-        N64ImageType m_ImageMode;
 
     public:
+        wxFileName               m_SourcePath;
+        wxBitmap                 m_Bitmap;
+        P64Img_Resize            m_ResizeMode;
+        wxPoint                  m_CustomSize;
+        P64Img_Alignment         m_Alignment;
+        P64Img_Fill              m_ResizeFill;
+        P64Img_Format            m_ImageFormat;
+        P64Img_Tiling            m_TilingX;
+        P64Img_Tiling            m_TilingY;
+        wxPoint                  m_MaskStart;
+        bool                     m_UseMipmaps;
+        P64Img_QuantizationMode  m_Quantization;
+        P64Img_AlphaMode         m_AlphaMode;
+        wxColor                  m_AlphaColor;
+        wxFileName               m_AlphaPath;
+        wxBitmap                 m_BitmapAlpha;
+        wxColor*                 m_FinalTexels;
+
         P64Asset_Image();
         ~P64Asset_Image();
+        wxColor* RegenerateFinal();
+        std::vector<uint8_t> Serialize();
+        static P64Asset_Image Deserialize(std::vector<uint8_t> bytes);
 };
