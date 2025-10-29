42466ed11ed167feb7eae0bd7c44e5a0b6698646
Buu342 <buu342@hotmail.com>
2025-10-29 16:07:53 +0000

    Sound browser documentation tweaks

--- Tools/SDK/Tools/Sound/frame_sound.cpp
+++ Tools/SDK/Tools/Sound/frame_sound.cpp
@@ -122,14 +122,19 @@ Frame_SoundBrowser::Frame_SoundBrowser(wxWindow* parent, wxWindowID id, const wx
     this->m_AssetModified = false;
     this->m_AssetFilePath = wxFileName("");
 
+    // Initialize the frame
     this->SetSizeHints(wxDefaultSize, wxDefaultSize);
 
+    // Create the main sizer
     wxBoxSizer* Sizer_Main;
     Sizer_Main = new wxBoxSizer(wxVERTICAL);
 
+    // Split the editor panel vertically
     this->m_Splitter_Vertical = new wxSplitterWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_3D|wxSP_LIVE_UPDATE);
     this->m_Splitter_Vertical->Connect(wxEVT_IDLE, wxIdleEventHandler(Frame_SoundBrowser::m_Splitter_VerticalOnIdle), NULL, this);
+    this->m_Splitter_Vertical->SetMinimumPaneSize(1);
 
+    // Setup the search panel
     this->m_Panel_Search = new Panel_Search(this->m_Splitter_Vertical, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
     this->m_Panel_Search->SetAssetType(CONTENT_NAME, CONTENT_EXTENSION);
     this->m_Panel_Search->SetTargetFrame(this);
@@ -139,56 +144,60 @@ Frame_SoundBrowser::Frame_SoundBrowser(wxWindow* parent, wxWindowID id, const wx
     this->m_Panel_Search->SetIconGenerator(IconGenerator);
     this->m_Panel_Search->SetMainFolder(((Frame_Main*)this->GetParent())->GetAssetsPath() + CONTENT_FOLDER + wxFileName::GetPathSeparator());
 
+    // Create the settings editor panel
     this->m_Panel_Edit = new wxPanel(this->m_Splitter_Vertical, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+
+    // Create the sizer for the settings editor
     wxBoxSizer* Sizer_Edit;
     Sizer_Edit = new wxBoxSizer(wxVERTICAL);
 
+    // Split the settings editor panel horizontally
     this->m_Splitter_Horizontal = new wxSplitterWindow(this->m_Panel_Edit, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_3D);
     this->m_Splitter_Horizontal->Connect(wxEVT_IDLE, wxIdleEventHandler(Frame_SoundBrowser::m_Splitter_HorizontalOnIdle), NULL, this);
+    this->m_Splitter_Horizontal->SetMinimumPaneSize(1);
 
+    // Add the preview panel and sizer
     this->m_Panel_Preview = new wxPanel(this->m_Splitter_Horizontal, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
     wxBoxSizer* Sizer_Preview;
     Sizer_Preview = new wxBoxSizer(wxVERTICAL);
+
+    // Setup the preview toolbar
     this->m_ToolBar_Preview = new wxToolBar(this->m_Panel_Preview, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTB_HORIZONTAL);
     this->m_ToolBar_Preview->Disable();
-
     this->m_Tool_Save = this->m_ToolBar_Preview->AddTool(wxID_ANY, wxEmptyString, Icon_Save, wxNullBitmap, wxITEM_NORMAL, _("Save changes"), wxEmptyString, NULL);
-
     this->m_ToolBar_Preview->AddSeparator();
-
     this->m_Tool_FlashcartUpload = this->m_ToolBar_Preview->AddTool(wxID_ANY, wxEmptyString, Icon_USBUpload, wxNullBitmap, wxITEM_NORMAL, _("Upload texture to flashcart"), wxEmptyString, NULL);
-
     this->m_ToolBar_Preview->Realize();
-
     Sizer_Preview->Add(this->m_ToolBar_Preview, 0, wxEXPAND, 5);
 
+    // Create the preview window
     this->m_ScrolledWin_Preview = new wxScrolledWindow(m_Panel_Preview, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxFULL_REPAINT_ON_RESIZE | wxHSCROLL | wxVSCROLL);
     this->m_ScrolledWin_Preview->SetScrollRate(5, 5);
     Sizer_Preview->Add(this->m_ScrolledWin_Preview, 1, wxEXPAND, 5);
-    
+
+    // Create the configuration panel
     this->m_Panel_Config = new wxPanel(this->m_Splitter_Horizontal, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_THEME|wxTAB_TRAVERSAL);
     this->m_Panel_Config->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW));
-
     wxFlexGridSizer* Sizer_Config;
     Sizer_Config = new wxFlexGridSizer(0, 2, 0, 0);
     Sizer_Config->AddGrowableCol(1);
     Sizer_Config->SetFlexibleDirection(wxBOTH);
     Sizer_Config->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
 
+    // Sound path file picker
     wxStaticText* StaticText_Path;
     StaticText_Path = new wxStaticText(this->m_Panel_Config, wxID_ANY, _("Sound path:"), wxDefaultPosition, wxDefaultSize, 0);
     StaticText_Path->Wrap(-1);
     Sizer_Config->Add(StaticText_Path, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
-
     this->m_FilePicker_Source = new wxFilePickerCtrl(this->m_Panel_Config, wxID_ANY, wxEmptyString, _("Select a file"), _("*.wav"), wxDefaultPosition, wxDefaultSize, wxFLP_DEFAULT_STYLE|wxFLP_OPEN|wxFLP_USE_TEXTCTRL);
     this->m_FilePicker_Source->Disable();
     Sizer_Config->Add(this->m_FilePicker_Source, 0, wxALL|wxEXPAND, 5);
 
+    // Sample rate choice box
     wxStaticText* StaticText_SampleRate;
     StaticText_SampleRate = new wxStaticText(this->m_Panel_Config, wxID_ANY, _("Sample Rate:"), wxDefaultPosition, wxDefaultSize, 0);
     StaticText_SampleRate->Wrap(-1);
     Sizer_Config->Add(StaticText_SampleRate, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
-
     wxString SampleRateChoices[] = { _("44100 Hz"), _("32000 Hz"), _("22050 Hz"), _("16000 Hz"), _("11025 Hz") };
     int SampleRateNChoices = sizeof(SampleRateChoices)/sizeof(wxString);
     this->m_Choice_SampleRate = new wxChoice(this->m_Panel_Config, wxID_ANY, wxDefaultPosition, wxDefaultSize, SampleRateNChoices, SampleRateChoices, 0);
@@ -196,56 +205,52 @@ Frame_SoundBrowser::Frame_SoundBrowser(wxWindow* parent, wxWindowID id, const wx
     this->m_Choice_SampleRate->Disable();
     Sizer_Config->Add(this->m_Choice_SampleRate, 0, wxALL, 5);
 
+    // Mono checkbox
     this->m_CheckBox_Mono = new wxCheckBox(this->m_Panel_Config, wxID_ANY, _("Force Mono"), wxDefaultPosition, wxDefaultSize, 0);
     this->m_CheckBox_Mono->SetValue(true);
     this->m_CheckBox_Mono->Disable();
     Sizer_Config->Add(this->m_CheckBox_Mono, 0, wxALL, 5);
 
+    // Loop checkbox
     Sizer_Config->Add(0, 0, 1, wxEXPAND, 5);
     this->m_CheckBox_Loop = new wxCheckBox(this->m_Panel_Config, wxID_ANY, _("Loop"), wxDefaultPosition, wxDefaultSize, 0);
     this->m_CheckBox_Loop->Disable();
-
     Sizer_Config->Add(this->m_CheckBox_Loop, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
 
+    // Loop start+end spin controls
     wxFlexGridSizer* Sizer_LoopPoints;
     Sizer_LoopPoints = new wxFlexGridSizer(0, 4, 0, 0);
     Sizer_LoopPoints->AddGrowableCol(1);
     Sizer_LoopPoints->AddGrowableCol(3);
     Sizer_LoopPoints->SetFlexibleDirection(wxBOTH);
     Sizer_LoopPoints->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
-
     wxStaticText* StaticText_LoopStart;
     StaticText_LoopStart = new wxStaticText(this->m_Panel_Config, wxID_ANY, _("Start:"), wxDefaultPosition, wxDefaultSize, 0);
     StaticText_LoopStart->Wrap(-1);
     Sizer_LoopPoints->Add(StaticText_LoopStart, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
-
     this->m_SpinCtrl_LoopStart = new wxSpinCtrl(this->m_Panel_Config, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, 10, 0);
     this->m_SpinCtrl_LoopStart->Disable();
-
     Sizer_LoopPoints->Add(this->m_SpinCtrl_LoopStart, 0, wxALL | wxEXPAND, 5);
-
     wxStaticText* StaticText_LoopEnd;
     StaticText_LoopEnd = new wxStaticText(this->m_Panel_Config, wxID_ANY, _("End:"), wxDefaultPosition, wxDefaultSize, 0);
     StaticText_LoopEnd->Wrap(-1);
     Sizer_LoopPoints->Add(StaticText_LoopEnd, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
-
     this->m_SpinCtrl_LoopEnd = new wxSpinCtrl(this->m_Panel_Config, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, 10, 0);
     this->m_SpinCtrl_LoopEnd->Disable();
     Sizer_LoopPoints->Add(this->m_SpinCtrl_LoopEnd, 0, wxALL | wxEXPAND, 5);
-
     Sizer_Config->Add(Sizer_LoopPoints, 1, wxEXPAND, 5);
+
+    // Finalize the layout
     this->m_Panel_Config->SetSizer(Sizer_Config);
     this->m_Panel_Config->Layout();
     Sizer_Config->Fit(this->m_Panel_Config);
     this->m_Splitter_Horizontal->SplitHorizontally(this->m_Panel_Preview, this->m_Panel_Config, 0);
     Sizer_Edit->Add(this->m_Splitter_Horizontal, 1, wxEXPAND, 5);
-
     this->m_Panel_Edit->SetSizer(Sizer_Edit);
     this->m_Panel_Edit->Layout();
     Sizer_Edit->Fit(m_Panel_Edit);
     this->m_Splitter_Vertical->SplitVertically(this->m_Panel_Search, this->m_Panel_Edit, 219);
     Sizer_Main->Add(this->m_Splitter_Vertical, 1, wxEXPAND, 5);
-
     this->SetSizer(Sizer_Main);
     this->Layout();
     this->Centre(wxBOTH);
